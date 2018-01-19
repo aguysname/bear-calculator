6d5ad08677e79a2d9e7e648fba8f9c6852465f71
aguysname <35611441+aguysname@users.noreply.github.com>
2018-01-19 11:55:23 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,36 @@
+function say(txt) {
+  $("#container").html(txt);
+}
+
+function getRnd(min, max) {
+  return (Math.random() * (max-min)) + min;
+}
+
+$(document).ready(function(){  
+  $("#approve").submit(function(e){
+    e.preventDefault();
+  });
+  console.log($("#approve .submit").first());
+  $("#approve .submit").first().click(function(){
+    var val=$("#approve .text").first().val();
+    var num=Number(val);
+    
+    if (isNaN(num)) {
+      say('Invalid number given');
+      return;
+    }
+    
+    if (num < 0 || num > 100) {
+      say('Number must be within the range of 0 and 100');
+      return;
+    }
+    
+    var rnd=getRnd(0, 100);
+    if (num > rnd) {
+      say('bear approved.');
+    }
+    else {
+      say('bear denied.');
+    }
+  });
+});
